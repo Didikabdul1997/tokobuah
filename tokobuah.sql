4926cfeaa528b2798ff1ba08b15818ea8fc30419
Didik Abdul Mukmin <didik.abdul1997@gmail.com>
2019-06-12 19:46:19 +0700

    index.php

--- tokobuah.sql
+++ tokobuah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 12, 2019 at 08:13 AM
+-- Generation Time: Jun 12, 2019 at 07:45 PM
 -- Server version: 10.1.40-MariaDB-0ubuntu0.18.04.1
 -- PHP Version: 7.2.19-0ubuntu0.18.04.1
 
@@ -42,7 +42,8 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`product_id`, `name`, `price`, `image`, `description`) VALUES
 ('5d004e4ebf8b9', 'Mangga', 5000, '5d004e4ebf8b9.png', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
-('5d004f201e04d', 'Apel', 4000, '5d004f201e04d.jpg', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
+('5d004f201e04d', 'Apel', 4000, '5d004f201e04d.jpg', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
+('5d00686234c0b', 'Jeruk', 5000, '5d00686234c0b.png', 'Pada kode persiapan di atas, saya memilih semua elemen, karena hanya ada satu elemen dalam form. Selain itu, saya menghilangkan menubar dan mengatur temanya menggunakana modern. Informasi lebih lengkap dengan kode persiapan, mungkin bisa dibaca di');
 
 --
 -- Indexes for dumped tables
